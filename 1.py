4afb9a5f69fa974f5a1317df1c4e104683e57f7e
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2019-09-18 11:07:50 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,3 +2,8 @@ naam = "Femke Nijman"
 adres = "De Tol 42, Dinxperlo, Gelderland, 7091ZR"
 telefoonnummer = "061385191"
 studie = "Bio-informatica"
+
+print("Naam:", naam ,"\n",
+      "Adres:", adres, "\n",
+      "Telefoonnummer:", telefoonnummer, "\n",
+      "Studie:", studie, "\n")
